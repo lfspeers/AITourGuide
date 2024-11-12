1d82c7f87de862852833ad48514a8cce3d0552b5
Lee Speers <mezzo.speers@gmail.com>
2024-11-12 16:01:18 -0700

    Added the AI Vision Landmark Detection function. Updated README.md

---
+++ AzureAIServices/Vision.py
@@ -0,0 +1,46 @@
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
+import os
+import requests
+
+
+
+def identify_landmark(image_url):
+    KEY = os.environ["AI_MULTISERVICE_KEY"]
+    URL = f"https://eastus.api.cognitive.microsoft.com/vision/v3.2/models/Landmarks/analyze"
+
+    headers = {
+        "Ocp-Apim-Subscription-Key": KEY,
+    }
+
+    parameters = {
+        "visualFeatures": "Categories",
+        "details": "Landmarks"
+    }
+
+    body = {"url": image_url}
+
+    # May need to check the image to determine content type
+    response = requests.post(
+        url=URL,
+        headers=headers,
+        json=body
+    )
+
+    try:
+        landmark = response.json()['result']['landmarks'][0]['name']
+    except:
+        landmark = ""
+
+    return landmark
+
+
+
+
+if __name__ == "__main__":
+    image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg/375px-Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg"
+    image_url2 = "https://thehill.com/wp-content/uploads/sites/2/2022/08/CA_farm_08222022istock.jpg?w=1280"
+
+    landmark = identify_landmark(image_url)
+    print(landmark)
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # AITourGuide
 Azure AI Services powered tour guide.
+
+
+Steps
+1. Identify Landmark
+    * Models
+        * AI Vision - Landmark Detection
+        * GPT Multimodal - Image
+    * Compare responses for accuracy
+        * Default to GPT model if they don't match (could also prompt the user)
+    * Return
+        * Landmark name
+2. Select Tour Guide
+    * 3 Prebuilt personas
+        * System message
+            * Tone, language, topics
+        * Azure AI Voice
+3. Generate Tour Text
+    * Models
+        * Any GPT-4 model
+    * Parameters
+        * System Message
+        * Persona name
+        * Landmark name
+    * Return
+        * Tour guide text
+4. TTS Tour
+    * Parameters
+        * Tour guide text
+    * Model
+        * Speech - TTS
+        * OpenAI - Whisper
+    * Return
+        * Audio Guided Tour
